4c7495e58cd20aa355cfc55c000502248b2f51b0
Shiotsuki_ <564165781@qq.com>
2020-08-17 21:31:54 +0800

    feat：version0.1.3

---
+++ config/cameraAction.js
@@ -0,0 +1,7 @@
+const publicPath = 'https://shiotsukikaedesari.gitee.io/mmddisplaydevice/'
+module.exports = [
+  {
+    label: '彩虹节拍',
+    url: `${publicPath}resources/action/rainbowBeat/rainbowBeatCamera.vmd`
+  }
+]
